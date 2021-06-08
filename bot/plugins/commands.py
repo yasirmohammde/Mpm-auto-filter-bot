77459833f74f5c37dc1f9ba4549d72196ba325e9
yasirmohammde <75161805+yasirmohammde@users.noreply.github.com>
2021-06-09 01:55:54 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = f"{file_name} \n |@Mpm_Linkz| |@motionpicturcapt,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🗂JOIN OUR MAIN CHANNEL🗂', url="https://t.me/joinchat/lr71lRGXjxZmNmU5"
                                 )
                         ]
                     ]
@@ -50,14 +50,14 @@ async def start(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
-                caption = caption,
+                caption = f"{file_name} \n |@Mpm_Linkz|,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN OUR MAIN CHANNEL', url="https://t.me/MOTION_Archive"
                                 )
                         ]
                     ]
